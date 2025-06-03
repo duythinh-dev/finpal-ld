9a93c575d0b634b6957c126c43b56e5a277c1f7e
duythinh <thinhogg11@gmail.com>
2025-06-03 18:25:50 +0700

    updatE

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,36 @@
-import type React from "react"
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import "./globals.css"
-import { ThemeProvider } from "@/components/theme-provider"
+import type React from "react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "DataTecHub - Nền tảng huấn luyện phân tích dữ liệu",
-  description: "Nâng cao kỹ năng phân tích dữ liệu của bạn với các khóa học chuyên sâu",
-    generator: 'v0.dev'
-}
+  description:
+    "Nâng cao kỹ năng phân tích dữ liệu của bạn với các khóa học chuyên sâu",
+  generator: "v0.dev",
+  icons: "/images/logo-ld.svg",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="vi" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          enableSystem
+          disableTransitionOnChange
+        >
           {children}
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
